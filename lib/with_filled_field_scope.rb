33921aac47266a649029c997e0820c41eca2a12f
Marcin Nowicki <pr0d1r2@doubledrones.com>
2010-03-08 14:21:21 +0100

    The gem itself.

---
+++ lib/with_filled_field_scope.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'active_record'
+require 'searchlogic'
+
+module ActiveRecord
+  module WithFilledFieldScope
+    module Base
+
+      FIELD_TYPES_SUPPORTING_ONLY_NOT_NULL = [:date, :decimal]
+
+      def has_with_filled_field_scope(model = self)
+        alias_scope :with_filled_field, lambda { |field|
+          if field_support_not_blank?(field, model)
+            send("#{field.to_s}_not_blank")
+          else
+            send("#{field.to_s}_not_null")
+          end
+        }
+      end
+
+      def field_support_not_blank?(field, model = self)
+        !FIELD_TYPES_SUPPORTING_ONLY_NOT_NULL.include?(field_column_type(field))
+      end
+
+      def field_column_type(field, model = self)
+        model.columns_hash[field.to_s].type
+      end
+
+    end
+  end
+end
+
+ActiveRecord::Base.extend(ActiveRecord::WithFilledFieldScope::Base)
+
--- spec/with_filled_field_scope_spec.rb
+++ spec/with_filled_field_scope_spec.rb
@@ -1,7 +1,62 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'active_record_connectionless'
 
-describe "WithFilledFieldScope" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+class WithFilledFieldScopeModel < ActiveRecord::Base
+  has_with_filled_field_scope
 end
+
+describe 'WithFilledFieldScopeModel' do
+
+  describe 'field_column_type' do
+    before do
+      @name_column = mock(ActiveRecord::ConnectionAdapters::Column)
+      @columns_hash = {'name' => @name_column}
+    end
+
+    it 'should read column type from models column hash' do
+      @name_column.should_receive(:type).and_return(:name_column_type)
+      WithFilledFieldScopeModel.should_receive(:columns_hash).and_return(@columns_hash)
+      WithFilledFieldScopeModel.field_column_type(:name).should == :name_column_type
+    end
+
+    it 'should allow to change model' do
+      @name_column.should_receive(:type).and_return(:name_column_type)
+      changed_model = mock(ActiveRecord::Base)
+      changed_model.should_receive(:columns_hash).and_return(@columns_hash)
+      WithFilledFieldScopeModel.field_column_type(:name, changed_model).should == :name_column_type
+    end
+  end
+
+  describe 'field_support_not_blank?' do
+    it 'should be false for date' do
+      WithFilledFieldScopeModel.should_receive(:field_column_type).with(:field).and_return(:date)
+      WithFilledFieldScopeModel.field_support_not_blank?(:field).should be_false
+    end
+
+    it 'should be false for decimal' do
+      WithFilledFieldScopeModel.should_receive(:field_column_type).with(:field).and_return(:decimal)
+      WithFilledFieldScopeModel.field_support_not_blank?(:field).should be_false
+    end
+
+    it 'should be true for string' do
+      WithFilledFieldScopeModel.should_receive(:field_column_type).with(:field).and_return(:string)
+      WithFilledFieldScopeModel.field_support_not_blank?(:field).should be_true
+    end
+  end
+
+  describe 'with_filled_field scope' do
+    it 'should use not null scope when field does not support not blank' do
+      WithFilledFieldScopeModel.should_receive(:field_support_not_blank?).with(:field, WithFilledFieldScopeModel).and_return(false)
+      WithFilledFieldScopeModel.should_receive(:field_not_null)
+      WithFilledFieldScopeModel.with_filled_field(:field)
+    end
+
+    it 'should use not blank scope when field support not blank' do
+      WithFilledFieldScopeModel.should_receive(:field_support_not_blank?).with(:field, WithFilledFieldScopeModel).and_return(true)
+      WithFilledFieldScopeModel.should_receive(:field_not_blank)
+      WithFilledFieldScopeModel.with_filled_field(:field)
+    end
+  end
+
+end
+
